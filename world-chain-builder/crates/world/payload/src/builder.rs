b8bb1fa705a4ebe37e869618fd5ecfcf64af5872
0xKitsune <0xkitsune@protonmail.com>
2025-04-06 23:20:35 -0400

    wip: impl payload builder ctx for worldchainpayloadbuilderctx

--- world-chain-builder/crates/world/payload/src/builder.rs
+++ world-chain-builder/crates/world/payload/src/builder.rs
@@ -4,7 +4,8 @@ use alloy_network::{TransactionBuilder, TxSignerSync};
 use alloy_rlp::Encodable;
 use alloy_rpc_types_debug::ExecutionWitness;
 use alloy_signer_local::PrivateKeySigner;
-use eyre::eyre::eyre;
+use eyre::eyre::{eyre, Chain};
+use flashblocks_payload::builder::PayloadBuilderCtx;
 use op_alloy_rpc_types::OpTransactionRequest;
 use op_revm::OpContext;
 use reth::api::PayloadBuilderError;
@@ -772,6 +773,80 @@ where
             .inner
             .evm_config
             .create_block_builder(evm, self.inner.parent(), execution_ctx))
+    }
+}
+
+impl<Client> PayloadBuilderCtx for WorldChainPayloadBuilderCtx<Client>
+where
+    Client: StateProviderFactory
+        + BlockReaderIdExt<Block = Block<OpTransactionSigned>>
+        + ChainSpecProvider<ChainSpec = OpChainSpec>
+        + Clone,
+{
+    type Evm = Evm;
+    type ChainSpec = Chainspec;
+
+    fn parent(&self) -> &SealedHeader {
+        self.parent()
+    }
+
+    fn attributes(
+        &self,
+    ) -> &OpPayloadBuilderAttributes<TxTy<<Self::Evm as ConfigureEvm>::Primitives>> {
+        self.attributes()
+    }
+
+    fn extra_data(&self) -> Result<Bytes, PayloadBuilderError> {
+        self.extra_data()
+    }
+
+    fn best_transaction_attributes(&self, block_env: &BlockEnv) -> BestTransactionsAttributes {
+        self.best_transaction_attributes(block_env)
+    }
+
+    fn payload_id(&self) -> PayloadId {
+        self.payload_id()
+    }
+
+    fn is_holocene_active(&self) -> bool {
+        self.is_holocene_active()
+    }
+
+    fn is_better_payload(&self, total_fees: U256) -> bool {
+        self.is_better_payload(total_fees)
+    }
+
+    fn block_builder<'a, DB: Database>(
+        &'a self,
+        db: &'a mut State<DB>,
+    ) -> Result<
+        impl BlockBuilder<Primitives = <Self::Evm as ConfigureEvm>::Primitives> + 'a,
+        PayloadBuilderError,
+    >
+    where
+        DB: Database,
+        DB::Error: Send + Sync + 'static,
+    {
+        self.block_builder(db)
+    }
+
+    fn execute_sequencer_transactions(
+        &self,
+        builder: &mut impl BlockBuilder<Primitives = <Self::Evm as ConfigureEvm>::Primitives>,
+    ) -> Result<ExecutionInfo, PayloadBuilderError> {
+        self.execute_sequencer_transactions(builder)
+    }
+
+    fn execute_best_transactions(
+        &self,
+        info: &mut ExecutionInfo,
+        builder: &mut impl BlockBuilder<Primitives = <Self::Evm as ConfigureEvm>::Primitives>,
+        best_txs: impl PayloadTransactions<
+            Transaction: PoolTransaction<Consensus = TxTy<<Self::Evm as ConfigureEvm>::Primitives>>,
+        >,
+        _gas_limit: u64,
+    ) -> Result<Option<()>, PayloadBuilderError> {
+        self.execute_best_transactions(info, builder, best_txs)
     }
 }
 
